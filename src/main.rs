a6f6261c99802ef0ad34ec1fa40b018daa4f6cf1
Hibikine Kage <goodsmile314159@gmail.com>
2018-11-08 09:14:57 +0900

    code sample bot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+extern crate dotenv;
+#[macro_use]
+extern crate serenity;
+
+use dotenv::dotenv;
+use serenity::client::Client;
+use serenity::framework::standard::StandardFramework;
+use serenity::prelude::EventHandler;
+use std::env;
+
+struct Handler;
+
+impl EventHandler for Handler {}
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    let mut client = Client::new(&env::var("DISCORD_TOKEN").expect("token"), Handler)
+        .expect("Error creating client");
+    client.with_framework(
+        StandardFramework::new()
+            .configure(|c| c.prefix("~"))
+            .cmd("ping", ping),
+    );
+    if let Err(why) = client.start() {
+        println!("An error occurred while running the client: {:?}", why);
+    }
 }
+
+command!(ping(_context, message) {
+    let _ = message.reply("Pong!");
+});
